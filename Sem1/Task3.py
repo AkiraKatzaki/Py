49f8ed8b82a393c4514f6fbb141693feea7ec2a6
Akira Katzaki <akira.katzaki@gmail.com>
2022-05-29 14:22:05 +0500

    get task3, task4

--- Sem1/Task3.py
+++ Sem1/Task3.py
@@ -1,2 +1,12 @@
 #Подсчитать сумму цифр в вещественном числе.
 
+def count_digit(number):
+    count = 0
+    str_num = str(number)
+    for i in range(len(str_num)):
+        if str_num[i] != '.':
+            count += int(str_num[i])
+    return count
+
+
+print(count_digit(111.22))  # Пользователь вводит данные
--- Sem1/Task4.py
+++ Sem1/Task4.py
@@ -1,3 +1,16 @@
 #Написать программу получающую набор произведений чисел от 1 до N.
 #Пример: пусть N = 4, тогда
 #[ 1, 2, 6, 24 ]
+
+def get_n_numbers(num_n):
+    if num_n <= 0:
+        return []
+    list_n = [1]
+    product = 1
+    for i in range(2, num_n + 1):
+        product *= i
+        list_n.append(product)
+    return list_n
+
+
+print(get_n_numbers(9))
